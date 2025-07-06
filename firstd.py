7610a190df8b6b80a298e67f1ddc7e6d73d72e39
theutkarshyadav2006 <utkarshyadav092006@gmail.com>
2025-07-07 01:42:17 +0530

    firstd.py

--- firstd.py
+++ firstd.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 @app.route("/info)
 def lwinfo():
-	return " i am lucky from sikar"
+	return " i am UTKARSH YADAV"
 
 @app.route("/phone")
 def lwphone():
-	return "786766767"
+	return "88888888"
 app.run(host="0.0.0.0")
